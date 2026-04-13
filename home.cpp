8abcb9b9a371e1785adb4610cf037baf5e15349a
Jordan Flowers <jtf1231@gmail.com>
2026-04-12 23:19:23 -0500

    Initial commit - smart home controller

---
+++ home.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Room {
+public:
+    string roomName;
+    bool lightOn;
+    int temperature;
+    int humidity;
+    bool fanOn;
+    int fanSpeed;
+
+    Room(string Name, bool light, int temp, int hum, bool fan, int speed) {
+        roomName = Name;
+        lightOn = light;
+        temperature = temp;
+        humidity = hum;
+        fanOn = fan;
+        fanSpeed = speed;
+    }
+        
+};
+
+
+void printRoom(Room room) {
+    cout << boolalpha << room.roomName << " " << room.lightOn << " " << room.temperature << " " << room.humidity << " " << room.fanOn << " " << room.fanSpeed << endl;
+}
+
+bool checkFan(Room room) {
+    if (room.temperature > 24 || room.humidity > 65) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    Room livingRoom("Living Room",true, 22, 50, true, 3);
+    Room bedroom("Bedroom", false, 18, 40, false, 0);
+    Room kitchen("Kitchen", true, 26, 70, true, 1);
+    Room bathroom("Bathroom", true, 24, 60, true, 2);
+
+
+    Room rooms[4] = {livingRoom, bedroom, kitchen, bathroom};
+
+    for (int i = 0; i < 4; i++) {
+        cout << boolalpha << rooms[i].roomName << " fan needed: " << checkFan(rooms[i]) << endl;
+    }
+
+    return 0;
+}
